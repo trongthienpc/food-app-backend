7a824508ec36e8b3f56a85da6bf820610b5552b7
thientt <pcking60@gmail.com>
2022-10-17 09:35:04 +0700

    add_prisma

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,29 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
+const client_1 = require("@prisma/client");
 const app = (0, express_1.default)();
 exports.app = app;
+const prisma = new client_1.PrismaClient();
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-app.get("/", (req, res) => {
-    res.send("Hi there!");
-});
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const foods = yield prisma.foods.findMany({});
+    res.send(foods);
+}));
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Server is listening on ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 const app: Express = express();
+const prisma = new PrismaClient();
 import dotenv from "dotenv";
 dotenv.config();
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hi there!");
+app.get("/", async (req: Request, res: Response) => {
+  const foods = await prisma.foods.findMany({});
+  res.send(foods);
 });
 const port = process.env.PORT || 8080;
 app.listen(port, () => {

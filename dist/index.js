85c4d5013e4f5a4d386ef5fdb92f3e9e431e81a6
thientt <pcking60@gmail.com>
2022-10-21 16:54:39 +0700

    add_some_module

--- dist/index.js
+++ dist/index.js
@@ -1,35 +1,28 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
-const client_1 = require("@prisma/client");
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 exports.app = app;
-const prisma = new client_1.PrismaClient();
 const dotenv_1 = __importDefault(require("dotenv"));
-const authentication_1 = __importDefault(require("./routes/authentication"));
+const authentication_routes_1 = __importDefault(require("./routes/authentication.routes"));
+const index_routes_1 = __importDefault(require("./routes/index.routes"));
 dotenv_1.default.config();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)());
-app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    res.send("Server is started");
-}));
-app.use("/api/user", authentication_1.default);
+// app.use(cors({ origin: true, credentials: true }));
+app.use(express_1.default.static("static"));
+// routes
+app.use("/", index_routes_1.default);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(res.status(404).send("Sorry, the page was not found"));
+});
+app.use("/api", authentication_routes_1.default);
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Server is listening on ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
 import express, { Express, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 const app: Express = express();
-const prisma = new PrismaClient();
 import dotenv from "dotenv";
-import authenticationRouter from "./routes/authentication";
+import authenticationRouter from "./routes/authentication.routes";
+import indexRouter from "./routes/index.routes";
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
 // app.use(cors({ origin: true, credentials: true }));
 
-app.get("/", async (req: Request, res: Response) => {
-  res.send("Server is started");
-});
+app.use(express.static("static"));
 
-app.use("/api/user", authenticationRouter);
+// routes
+app.use("/", indexRouter);
+
+app.use("/api", authenticationRouter);
+
+// catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(res.status(404).send("Sorry, the page was not found"));
+// });
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is listening on ${port}`);
